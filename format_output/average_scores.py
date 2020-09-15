1b05f7cb470d6fcc7b20df366aa294bebb4939cf
ComputerUser158 <oneringraw@gmail.com>
2020-09-14 19:01:02 -0500

    first commit

---
+++ format_output/average_scores.py
@@ -0,0 +1,19 @@
+"""
+def average():
+    score1 = input("enter your first grade: ")
+    score2 = input("enter your second grade: ")
+    score3 = input("enter your third grade: ")
+    return(int(score1) + int(score2) + int(score3))/3
+"""
+
+
+def average():
+    pass
+
+
+if __name__ == "__main__":
+    lastName = input("enter your last name: ")
+    firstName = input("enter your first name: ")
+    age = input("enter your age: ")
+    #average_scores = average()
+    #print("{}, {} age: {} average grade: {}".format(lastName, firstName, age, average_scores))
